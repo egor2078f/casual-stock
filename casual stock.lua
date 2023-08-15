00dcdcc52ed442037ccb992397c28f30b7b36168
egor2078f <142328017+egor2078f@users.noreply.github.com>
2023-08-15 16:48:39 +0200

    Update casual stock.lua

--- casual stock.lua
+++ casual stock.lua
@@ -1,49 +1,17 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
---[[ 
-В данный момент стоит тема "RJTheme3" ,
-вы можете использовать другую тему приведённую ниже -
-"RJTheme1"
-"RJTheme2"
-"RJTheme3"
-"RJTheme4"
-"RJTheme5"
-"RJTheme6"
-"RJTheme7"
-"RJTheme8"
-//////////////////////////////////////////////////////////////////
 
-Что бы сделать свою тему , уберите часть скрипта из "комминтариев" ,
-который находится чуть ниже , и вместо "RJTheme3" в переменной "Windows" - 
-напишите переменную которая используется в скрипте чуть ниже .
-]]
 
---[[
-local colors = {
-	-- Цвет фона у Секций
-    SchemeColor = Color3.fromRGB(150, 72, 148),
-	-- Цвет фона в правой части UI
-	Background = Color3.fromRGB(15,15,15),
-	-- Цвет фона в левой части UI
-    Header = Color3.fromRGB(15,15,15),
-	-- Цвет текста
-    TextColor = Color3.fromRGB(255,255,255),
-	-- Цвет фона у кнопок
-    ElementColor = Color3.fromRGB(20, 20, 20)
-}
-]]
+local Window = Library.CreateLib("Casual Stock", "RJTheme6", true)
 
--- Создать окно UI
-local Window = Library.CreateLib("Casual Stock", "RJTheme6", true) -- добавьте параметр 'false' для отключения скруглений.
 
--- Секция
 local Tab = Window:NewTab("Functions")
 
 
--- Подсекция
+
 local Section = Tab:NewSection("Misc")
 
--- Переключатель
+
 Section:NewToggle("AutoBuy, Sport Master", "Auto Buy Sport Master", function(state)
     if state then
         print("Auto buy On")
@@ -52,7 +20,7 @@ Section:NewToggle("AutoBuy, Sport Master", "Auto Buy Sport Master", function(sta
     end
 end)
 
--- Переключатель
+
 Section:NewToggle("AutoBuy, Zara", "Auto Buy Zara", function(state)
     if state then
         print("Auto buy On")
@@ -74,7 +42,7 @@ Section:NewButton("Fps Unlocker", "+100 fps", function()
 end)
 
 -- Слайдер
-Section:NewSlider("Speed Player", "SliderInfo", 36, 16, function(s) -- 50 (Макс. значение) | 0 (Мин. значение)
+Section:NewSlider("Speed Player", "SliderInfo", 36, 16, function(s) 
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
